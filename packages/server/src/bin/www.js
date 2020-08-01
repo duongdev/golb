e45ad3c33ae46e2f973f44f87bc21af77980b81b
Dương Đỗ <dustin.do95@gmail.com>
2020-08-01 20:58:32 +0700

    Initialize monorepo packages (web, server) (#2)

---
+++ packages/server/src/bin/www.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+import app from '../app'
+import http from 'http'
+import Debug from 'debug'
+import mongoose from 'mongoose'
+
+const debug = Debug('server:www')
+
+const PORT = normalizePort(process.env.PORT || '3000')
+const MONGO_URI = 'mongodb://localhost:27017/blog'
+
+app.set('port', PORT)
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app)
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    debug(`Connected to MongoDB at ${MONGO_URI}`)
+  })
+  .then(() => {
+    server.listen(PORT)
+  })
+
+server.on('error', onError)
+server.on('listening', onListening)
+
+function normalizePort(val) {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    return val
+  }
+
+  if (port >= 0) {
+    return port
+  }
+
+  return false
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof PORT === 'string' ? 'Pipe ' + PORT : 'Port ' + PORT
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('Listening on ' + bind)
+}
