07a8365b3a7feb74a77ebb2e31d905581daaa60e
Dương Đỗ <dustin.do95@gmail.com>
2020-08-01 20:39:50 +0700

    init express with babel

---
+++ packages/server/dist/bin/www.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+"use strict";
+
+var _app = _interopRequireDefault(require("../app"));
+
+var _http = _interopRequireDefault(require("http"));
+
+var _debug = _interopRequireDefault(require("debug"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var debug = (0, _debug["default"])('server:www');
+var port = normalizePort(process.env.PORT || '3000');
+
+_app["default"].set('port', port);
+/**
+ * Create HTTP server.
+ */
+
+
+var server = _http["default"].createServer(_app["default"]);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port; // handle specific listen errors with friendly messages
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+
+    default:
+      throw error;
+  }
+}
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
