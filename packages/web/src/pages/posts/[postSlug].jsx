6506b20bd01efd0bab08fe91e4025d0b61deb604
Dương Đỗ <dustin.do95@gmail.com>
2020-08-03 11:00:30 +0700

    Add ManagePostActions

--- packages/web/src/pages/posts/[postSlug].jsx
+++ packages/web/src/pages/posts/[postSlug].jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import Layout from 'components/Layout'
-import { makeStyles, Container, Grid, Typography } from '@material-ui/core'
+import {
+  makeStyles,
+  Container,
+  Grid,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Box,
+} from '@material-ui/core'
 import Error from 'next/error'
 import { createClient } from 'api-client'
 import TextEditor from 'components/Editor'
 import UserDisplay from 'components/UserDisplay'
 import moment from 'moment'
+import { DotsVertical } from 'mdi-material-ui'
+import { useMemo } from 'react'
+import { useAuth } from 'contexts/AuthContext'
+import { useState } from 'react'
+import Link from 'next/link'
 
 const PostView = (props) => {
-  useStyles(props)
-
   const { post } = props
+  useStyles(props)
+  const currentUser = useAuth()
+
+  const isPostAuthor = useMemo(() => post?.createdBy?.id === currentUser?.id, [
+    currentUser.id,
+    post.createdBy.id,
+  ])
 
   if (!post) {
     return <Error statusCode={props.errorCode ?? 500} />
@@ -21,9 +40,18 @@ const PostView = (props) => {
       <Container maxWidth="md">
         <Grid container spacing={4} direction="column" wrap="nowrap">
           <Grid item>
-            <Typography gutterBottom variant="h3">
-              <strong>{post.title}</strong>
-            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm>
+                <Typography gutterBottom variant="h3">
+                  <strong>{post.title}</strong>
+                </Typography>
+              </Grid>
+              {isPostAuthor && (
+                <Grid item>
+                  <ManagePostActions postId={post.id} />
+                </Grid>
+              )}
+            </Grid>
           </Grid>
 
           {post.createdBy && (
@@ -49,6 +77,46 @@ const PostView = (props) => {
         </Grid>
       </Container>
     </Layout>
+  )
+}
+
+const ManagePostActions = (props) => {
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  return (
+    <>
+      <IconButton onClick={(event) => setAnchorEl(event.currentTarget)}>
+        <DotsVertical />
+      </IconButton>
+      <Menu
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>
+          <Link
+            passHref
+            href="/posts/edit/[postId]"
+            as={`/posts/edit/${props.postId}`}
+          >
+            <Box display="flex" component="a" alignItems="center">
+              Edit this post
+            </Box>
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Typography color="error">Delete this post</Typography>
+        </MenuItem>
+      </Menu>
+    </>
   )
 }
 
